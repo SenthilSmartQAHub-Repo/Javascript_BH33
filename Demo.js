7a6636da9cae81bf53b794f5db61dda5d7614239
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-03-01 12:15:05 +0530

    javascript init code

--- Demo.js
+++ Demo.js
@@ -14,3 +14,29 @@
 var studentName="Arun";
 console.log(studentName)
 
+//================== VariableName -  Naming convention  ------> Camel case=====================================
+
+// variable name---->  first word should be lowercase and 2nds word onward every word 1st letter should be uppercase
+
+    var userNameId
+    var userAccount_Number // _ is not recommended
+
+//=================================variable Rules==========================================================================
+
+//1 .  variable names can contain letters(a-z,A-Z,0-9,_,$) and (not allowed--> &,@,- and others).
+//2.   variable name can't start with a digit(0-9).
+//3.   variable can be start with a letter,_,$.
+//4.   variable case sensivity.
+
+//======================================================================================================================
+/*
+let 5date=20 invalue
+let date&Time=23.45 invalid
+let new="data" invalid
+let function=20; invalid 
+let #value="welcome" invalid
+let @value="playwright"  invalid
+let $value="javascript" valid
+let _value="typescript" valid
+/*
+
