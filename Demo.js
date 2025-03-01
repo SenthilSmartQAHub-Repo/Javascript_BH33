3031281639780fd8f3e31d2bdf8babff2dd10950
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-03-01 12:05:47 +0530

    javascript init code

---
+++ Demo.js
@@ -0,0 +1,16 @@
+//Javascript variables----> used to store the value
+
+// 3 ways variables can be declared
+// 1. Using Var
+// 2. Using let
+// 3. Using const
+
+//Variable Declaration
+//var studentName
+
+//Variable Initialization
+//studentName="Arun"
+
+var studentName="Arun";
+console.log(studentName)
+
